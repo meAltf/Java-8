8a425d524159ed8a1d07d04a7fb24c9f904abc64
meAltf <alataf.dce.cs1@gmail.com>
2023-11-07 11:59:45 +0530

    Implement function interface

---
+++ src/FunctionalInterfaces/FunctionExample1.java
@@ -0,0 +1,15 @@
+package FunctionalInterfaces;
+
+public class FunctionExample1 {
+
+    public static String performConcat(String str){
+
+        return FunctionExample.addSomeString.apply(str);
+    }
+
+    public static void main(String[] args) {
+        String result = performConcat("Alataf");
+
+        System.out.println("Result is : " + result);
+    }
+}
