fc5b406553a19092435a3d3be771edf2cef9ca21
meAltf <alataf.dce.cs1@gmail.com>
2023-11-07 17:45:53 +0530

    restrictions on lambdaVariables

---
+++ src/lambdas/LambdaVariable2.java
@@ -0,0 +1,20 @@
+package lambdas;
+
+import java.util.function.Consumer;
+
+public class LambdaVariable2 {
+    static int value = 4;
+
+    public static void main(String[] args) {
+
+        int value =4; //effectively final
+        Consumer<Integer> c1 = (a) -> {
+            // value = 6;
+            //value++; -- its gives error also
+            //  System.out.println(i+value);
+        };
+        //value =2;
+
+        c1.accept(2);
+    }
+}
