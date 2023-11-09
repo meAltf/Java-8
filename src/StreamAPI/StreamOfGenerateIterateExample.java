5d4f4a4be56254114657c81c5f91c16510fc5809
meAltf <alataf.dce.cs1@gmail.com>
2023-11-09 17:00:00 +0530

    Implemented StreamOf generate and iterate stream

---
+++ src/StreamAPI/StreamOfGenerateIterateExample.java
@@ -0,0 +1,50 @@
+package StreamAPI;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Streams API - of() | iterate() | generate()
+ *
+ * of() - > Creates a stream of certain values passed to this method
+ * Example-
+ *  Stream<String> stringStream = Stream.of("alataf", "ansari", "warish");
+ *
+ *  iterate(), generate() -> USed to create infinite Streams.
+ *  Example-
+ *  Stream.iterate(1, x->x*2)
+ *
+ *  Example-
+ *  Stream.generate(<Supplier>)
+ *
+ */
+public class StreamOfGenerateIterateExample {
+
+    public static void main(String[] args) {
+
+        Stream<String> stringStream = Stream.of("adam","dan","jenny","dave");
+        stringStream.forEach(System.out::println);
+
+        List<Integer> integerList  = Stream.iterate(1, x->x*2)
+                .limit(10)
+               // .map(Integer::new)
+                .collect(toList());
+
+        System.out.println("iterate : " + integerList);
+
+
+        Supplier<Integer> supplier = new Random()::nextInt;
+
+        List<Integer> integerList1  = Stream.generate(supplier)
+                .limit(10)
+                .collect(toList());
+        System.out.println("generate : " + integerList1);
+
+    }
+
+
+}
